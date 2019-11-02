2209b4bc97c55cf5d9a8ec3449e0cfdbd496fc7d
Michael Mottola <mottola64@gmail.com>
2019-11-01 22:56:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+
+function fetchDogs() {
+  return fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => renderDogs(json));
+}
+
+function renderDogs(json) {
+  const dogImageContainer = document.querySelector("#dog-image-container");
+  json.message.forEach(dog => {
+    const img = document.createElement("img");
+    img.src = dog;
+    dogImageContainer.appendChild(img);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetchDogs();
+});
+
+function fetchBreeds() {
+  return fetch("https://dog.ceo/api/breeds/list/all")
+    .then(resp => resp.json())
+    .then(json => renderBreeds(json));
+}
+
+function renderBreeds(json) {
+  console.log(json);
+  const dogBreeds = document.querySelector("#dog-breeds");
+  json.message.forEach(dog => {
+    const breed = document.createElement("breed");
+    breed.src = dog;
+    dogBreeds.appendChild(breed);
+  });
+}
+
+document.addEventListener("DomContentLoaded", function() {
+  fetchBreeds();
+});
